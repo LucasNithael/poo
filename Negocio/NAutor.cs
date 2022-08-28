7cb9676ebf37aaf1885aa216e8379d8f8c582678
Lucas Nithael <94084548+LucasNithael@users.noreply.github.com>
2022-08-28 21:13:29 +0000

    arquivos

--- Negocio/NAutor.cs
+++ Negocio/NAutor.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 
 static class NAutor{
-  private  static List<Autor> autores = new List<Autor>();
+  private  static List<Autor> autores;
   public static void Inserir(Autor obj){
+    autores = Abrir();
     int id = 0;
     foreach(Autor i in autores)
       if(i.Id > id) id = i.Id;
     id++;
     obj.Id = id;
     autores.Add(obj);
+    Salcar(autores);
   }
   public static List<Autor> Listar(){
-    return autores.OrderBy(obj => obj.Id).ToList();
+    autores = Abrir();
+    if(autores.Count == 0) throw new NullReferenceException("Não existe Gêneros");
+    else return autores;
   }
   public static void Atualizar(Autor obj){
     Autor x = Pesquisar(obj.Id);
